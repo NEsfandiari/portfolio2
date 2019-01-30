b781991cba6782abad9b0313e0dabe7491512b09
NEsfandiari <nesfandiari37@gmail.com>
2019-01-29 17:09:59 -0800

    style tweaks

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,10 +21,21 @@ const Container = styled.div`
     margin-top: 10rem;
     color: white;
     max-width: 15rem;
-    font-size: 0.9rem;
+    font-size: 1rem;
     a {
       text-decoration: none;
       color: white;
+      :hover {
+        color: lightgray;
+        text-decoration: azure underline;
+      }
+    }
+    h5 {
+      line-height: 1.3rem;
+    }
+    p {
+      line-height: 1.3rem;
+      font-size: 0.85rem;
     }
   }
   h4 {
@@ -79,15 +90,17 @@ class projects extends Component {
               <p data-aos="fade-in" data-aos-delay="100">
                 {info.blurb}
               </p>
-              <a
-                href={info.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                Link
-              </a>
+              <h5>
+                <a
+                  href={info.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  data-aos="fade-up"
+                  data-aos-delay="200"
+                >
+                  Link
+                </a>
+              </h5>
             </div>
           ) : (
             ''
@@ -106,26 +119,25 @@ projects.defaultProps = {
     blurb:
       'Birthed in the aftermath of the 2016 presidential election, Lipslut is a politcally charged cosmetics brand vowing for justice by donating 50% of profits to charity. I created the web app to replatorm them off of Squarespace.',
     technologies:
-      'Gatsby.js, React.js, AWS Lambda, Firebase, Contentful, Stripe, Shopify, GraphQL',
+      'React.js, AWS Lambda, Firebase, Contentful, Stripe, Shopify, GraphQL',
   },
   groupmuse: {
     link: 'https://itunes.apple.com/us/app/groupmuse/id942675649',
     blurb:
       'An online social network that connects young classical musicians to local audiences through concert house parties. I worked on their IOS app.',
-    technologies: 'React-Native, Redux, Redux-Thunk, Storybook, Stripe, Ruby',
+    technologies: 'React-Native, Redux, Storybook, Stripe, Ruby',
   },
   cherries: {
     link: 'https://jamstack-cherries.netlify.com/',
     blurb:
-      'A fake E-commerce brand I developed and used to test many of the technologies (in the JAMstack paradigm) that I used while working for Lipslut.',
-    technologies:
-      'Gatsby.js, React.js, Fuse.js, AWS Lambda, Contentful, Shopify, GraphQL',
+      'A fake E-commerce brand I developed and used to test many of the technologies (JAMstack paradigm) that I used while working for Lipslut.',
+    technologies: 'React.js, Fuse.js, AWS Lambda, Contentful, Shopify, GraphQL',
   },
   warbler: {
     link: 'https://warbler-project.herokuapp.com/',
     blurb:
       'A Twitter clone with a new face! Users can create profiles, add messages, and interact with other users.',
-    technologies: 'Python3, Flask, jQuery, JWT',
+    technologies: 'Python, Flask, jQuery, JWT',
   },
   memory: {
     link: 'https://nikiesfandiari-memory-game.herokuapp.com/index.html',
